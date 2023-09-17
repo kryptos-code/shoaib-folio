bbc323a9160f63f37f6a7e8289adc1aadaacbd98
Shoaib Rehman <kryptosshoaib@gmail.com>
2023-09-17 18:28:34 +0530

    Fixed typed usage for auto texting

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
-var typed = new Typed(".multiple-text", {
-    strings: ["Software Engineer", "Java Developer", "Content Creator", "Blogger"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-}); 
+function getTyped() {
+    var typed = new Typed(".multiple-text", {
+        strings: ["Software Engineer", "Java Developer", "Content Creator", "Blogger"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });     
+}
+
+function changeActive(name) {
+    
+    var activeClasses = document.getElementsByClassName('active');
+    while (activeClasses.length)
+        activeClasses[0].classList.remove('active');
+
+    var element2 = document.getElementById('nav-'+name);
+    element2.classList.add('active');
+}
